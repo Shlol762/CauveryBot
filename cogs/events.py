5f819299f815421cd231a4c14cdd629f25a4ae26
Shlok <wizard1net@gmail.com>
2021-11-18 20:08:32 +0530

    Added logging, `on_shard_connect` event, `on_shard_disconnect` event.

--- cogs/events.py
+++ cogs/events.py
@@ -1,11 +1,31 @@
 from cogs import Cog, CommandError, CommandOnCooldown,\
     CommandNotFound, AutoShardedBot
+from datetime import datetime
+
+
+LOGS = {
+    'shards': 'C:/Users/Shlok/CauveryBot/logs/shards'
+}
 
 
 class Events(Cog):
     def __init__(self, bot: AutoShardedBot):
         self.bot = bot
         self.name = 'Events'
+        self.shard: int = 0
+
+    @Cog.listener('on_shard_connect')
+    async def on_shard_connect(self, id: int):
+        with open(LOGS['shards'], 'a') as f:
+            self.shard = id
+            print(f"Connection to shard: {id} at {datetime.now().strftime('%d %B %Y at %X:%f')}", file=f)
+
+    @Cog.listener('on_shard_disconnect')
+    async def on_shard_disconnect(self, id: int):
+        with open(LOGS['shards'], 'a') as f:
+            print(f'WARNING! Shard continuity fail. {id} != {self.shard}', file=f) if id != self.shard else None
+            print(f"Disconnected shard : {id} on {datetime.now().strftime('%d %B %Y at %X:%f')}", file=f)
+
 
     @Cog.listener('on_ready')
     async def on_ready(self):
