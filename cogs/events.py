392a26ce8fd2887a00f1c4a3b39c8ba6a3433904
Shlok <wizard1net@gmail.com>
2021-11-19 12:28:02 +0530

    Added `on_message` and `on_message_delete` events.

--- cogs/events.py
+++ cogs/events.py
@@ -1,11 +1,15 @@
 from cogs import Cog, CommandError, CommandOnCooldown,\
-    CommandNotFound, AutoShardedBot
-from datetime import datetime
+    CommandNotFound, AutoShardedBot, Message, AuditLogAction,\
+    AuditLogEntry, Member
+from datetime import datetime, timezone
+from pytz import timezone as tz
 
 
 LOGS = {
-    'shards': 'C:/Users/Shlok/CauveryBot/logs/shards'
+    'shards': 'C:/Users/Shlok/CauveryBot/logs/shards',
+    'del messages': 'C:/Users/Shlok/CauveryBot/logs/msgdel'
 }
+TIM_FMT = '%d %B %Y at %X:%f'
 
 
 class Events(Cog):
@@ -18,23 +22,35 @@ class Events(Cog):
     async def on_shard_connect(self, id: int):
         with open(LOGS['shards'], 'a') as f:
             self.shard = id
-            print(f"Connection to shard: {id} at {datetime.now().strftime('%d %B %Y at %X:%f')}", file=f)
+            print(f"Connection to shard: {id} on {datetime.now().strftime(TIM_FMT)}", file=f)
 
     @Cog.listener('on_shard_disconnect')
     async def on_shard_disconnect(self, id: int):
         with open(LOGS['shards'], 'a') as f:
-            print(f"Disconnected shard : {id} on {datetime.now().strftime('%d %B %Y at %X:%f')}", file=f)
+            print(f"Disconnected shard : {id} on {datetime.now().strftime(TIM_FMT)}", file=f)
 
     @Cog.listener('on_shard_resumed')
     async def on_shard_resumed(self, id: int):
         with open(LOGS['shards'], 'a') as f:
             self.shard = id
             print(f'WARNING! Shard continuity fail. {id} != {self.shard}', file=f) if id != self.shard else None
-            print(f"Reconnection shard : {id} at {datetime.now().strftime('%d %B %Y at %X:%f')}", file=f)
+            print(f"Reconnection shard : {id} on {datetime.now().strftime(TIM_FMT)}", file=f)
 
     @Cog.listener('on_ready')
     async def on_ready(self):
         print(f"{self.bot.user.name} is ready!")
+
+    @Cog.listener('on_message')
+    async def on_message(self, message: Message):
+        pass
+
+    @Cog.listener('on_message_delete')
+    async def on_message_delete(self, message: Message):
+        time = time.strftime(TIM_FMT)
+        with open(LOGS['del messages'], 'a') as f:
+            print(f'Message {message.id}:\n\tAuthor  - {message.author.id}'
+                  f'\n\tContent - {message.content}\n\tTime    - {time}',
+                  file=f)
 
 
 
